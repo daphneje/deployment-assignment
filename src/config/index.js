46bf3351ba44e8d67f7688acee998be3f569a26b
daphneje <91236787+daphneje@users.noreply.github.com>
2021-11-29 16:23:11 +0800

    test deploy

---
+++ src/config/index.js
@@ -0,0 +1,18 @@
+const env = process.env.REACT_APP_ENV;
+
+let config = {
+    baseUrl: "http://localhost:3000"
+};
+
+if (env) {
+    switch (env.toUpperCase()) {
+        case "STAGE":
+            config.baseUrl = "http://stage.localhost/";
+            break;
+        case "PRODUCTION":
+            config.baseUrl = "http://localhost.com/"
+            break;
+    }
+}
+
+export default config;
